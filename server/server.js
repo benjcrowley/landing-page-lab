e3d8b246cf9028f2dbf53c3be29443bd1e389eae
Bennett Crowley <benjcrowley@gmail.com>
2022-11-02 14:30:46 -0600

    ready to deploy the shell

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+require("dotenv").config()
+
+const app = express()
+
+app.use(express.json())
+
+const {home} = require("./controllers/pageCtrl")
+
+app.get("/", home)
+
+const { PORT } = process.env
+
+app.listen(PORT, () => console.log(`server is running on ${PORT}`))
